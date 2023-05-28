361387e130d941a15e342f31191d663a774151ab
Joppen George <joppenmaliakal@gmail.com>
2023-05-28 20:01:56 +0530

    ui of some settings changed

--- lib/all_settings/userprofile.dart
+++ lib/all_settings/userprofile.dart
@@ -3,153 +3,229 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ui/all_settings/editprofile.dart';
 
-
 class Userdetails extends StatefulWidget {
   const Userdetails({super.key});
-
 
   @override
   State<Userdetails> createState() => _UserdetailsState();
 }
 
 class _UserdetailsState extends State<Userdetails> {
-
-  String? name='';
-  String? email='';
-  String? phone='';
+  String? name = '';
+  String? email = '';
+  String? phone = '';
   bool? isLoading;
 
-  Future getdetails() async
-  {
+  Future getdetails() async {
     setState(() {
-    isLoading = true; // set the isLoading state to true
-  });
-    await FirebaseFirestore.instance.collection("users")
-    .doc(FirebaseAuth.instance.currentUser!.uid)
-    .get()
-    .then((snapshot) async
-    {
-      if(snapshot.exists)
-      {
+      isLoading = true; // set the isLoading state to true
+    });
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((snapshot) async {
+      if (snapshot.exists) {
         setState(() {
-          email=snapshot.data()!["Email"];
-          phone=snapshot.data()!["Phone"];
-         name= snapshot.data()!["Name"];
+          email = snapshot.data()!["Email"];
+          phone = snapshot.data()!["Phone"];
+          name = snapshot.data()!["Name"];
         });
       }
-      
-    } 
-    );
+    });
 
     setState(() {
-    isLoading = false; // set the isLoading state to false
-  });
+      isLoading = false; // set the isLoading state to false
+    });
   }
+
   @override
   void initState() {
-    
-    // TODO: implement initState
     super.initState();
     getdetails();
   }
 
-
   @override
   Widget build(BuildContext context) {
     if (isLoading!) {
-  return Container(color: Colors.white,
-    child: const Center(
-      child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation(Color.fromARGB(255, 71, 68, 214)),
-      ),
-    ),
-  );
-} else {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70,
-        backgroundColor: const Color.fromARGB(255, 71, 68, 214),
-        title: const Text('User Profile'),
-        
+      return Container(
+        color: Colors.white,
+        child: const Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation(
+              Color.fromARGB(234, 11, 11, 11),
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: const Color.fromARGB(234, 11, 11, 11),
+          title: const Text(
+            'User Profile',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           actions: [
-          IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () {
-              Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const EditUserDetailsPage()));
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Name :',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const EditUserDetailsPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            Container(
+              color: const Color.fromARGB(234, 11, 11, 11),
+            ),
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+                child: Container(
+                  color: Colors.white,
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    const Icon(Icons.person),
+                                    const SizedBox(width: 15),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            'Name:',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 8),
+                                          Text(
+                                            name!,
+                                            style: const TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w300,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              Container(
+                                padding: const EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    const Icon(Icons.phone),
+                                    const SizedBox(width: 15),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            'Phone Number:',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 8),
+                                          Text(
+                                            phone!,
+                                            style: const TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w300,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              Container(
+                                padding: const EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                                child: Row(
+                                  children: [
+                                    const Icon(Icons.email),
+                                    const SizedBox(width: 15),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            'Email:',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 8),
+                                          Text(
+                                            email!,
+                                            style: const TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w300,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              name!,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-                
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Phone Number :',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              phone!,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Email :',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              email!,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+          ],
+        ),
+      );
+    }
   }
 }
